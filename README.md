a3241be5127b0b7dcf38d0d57fe554761e6aa053
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2018-12-22 04:49:04 +0630

    release

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # facebook-video-downloader
+
+## Installation
+
+    npm i facebook-video-downloader
+
+## Usage
+
+```js
+'use strict';
+
+const getDownloadUrl = require('facebook-video-downloader');
+
+getDownloadUrl('https://www.facebook.com/facebook/videos/10153231379946729/')
+  .then(console.log)
+  .catch(console.error);
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const axios = require('axios');
+
+function getDownloadUrl(url) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { data } = await axios(url);
+      const hd_src = data.match(/hd_src:"([\s\S]*?)"/)[1];
+      const hd_size = parseInt((await axios.head(hd_src)).headers['content-length']);
+      const sd_src = data.match(/sd_src:"([\s\S]*?)"/)[1];
+      const sd_size = parseInt((await axios.head(sd_src)).headers['content-length']);
+      resolve({
+        hd: {
+          url: hd_src,
+          fileSize: hd_size
+        },
+        sd: {
+          url: sd_src,
+          fileSize: sd_size
+        }
+      });
+      resolve(hd_size);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+module.exports = getDownloadUrl;
